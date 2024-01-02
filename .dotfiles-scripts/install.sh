158d6777c6baac4858bf50c71fd2ed2cfe08818f
Clémence Frioux <cfrioux@users.noreply.github.com>
2024-01-02 18:24:25 +0100

    personalization

--- .dotfiles-scripts/install.sh
+++ .dotfiles-scripts/install.sh
@@ -11,10 +11,10 @@ then
   dotfiles pull --quiet
 else
   echo ".cfg" >> .gitignore
-  git clone https://github.com/lrivallain/dotfiles.git $HOME/.cfg --quiet
+  git clone https://github.com/cfrioux/dotfiles.git $HOME/.cfg --quiet
   dotfiles config --local status.showUntrackedFiles no
   dotfiles checkout -f --quiet
 fi
 
 bash $HOME/.dotfiles-scripts/zsh-setup.sh
-bash $HOME/.dotfiles-scripts/tools-setup-apt.sh
+#bash $HOME/.dotfiles-scripts/tools-setup-apt.sh
